c4be5a0af09ae81969f70166b0c669cc98acfe7c
meganallred <meg21allred@gmail.com>
2020-03-02 22:28:28 -0500

    adding new recipe button

--- app/src/main/java/org/byui/meg21allred/thymesavor/MainActivity.java
+++ app/src/main/java/org/byui/meg21allred/thymesavor/MainActivity.java
@@ -2,15 +2,30 @@ package org.byui.meg21allred.thymesavor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button enterNewRecipeBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("This is our App");
+        enterNewRecipeBtn = (Button) findViewById(R.id.enterNewRecipeBtn);
+    }
+
+    public void newRecipe(View v) {
+        openNewRecipeActivity();
+    }
+
+    public void openNewRecipeActivity() {
+        Intent intent = new Intent(this, NewRecipeActivity.class);
+        startActivity(intent);
+
     }
 }
