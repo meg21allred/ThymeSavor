d4d1dcc8509dbee1ecb1068fb1585baf9289694a
meganallred <meg21allred@gmail.com>
2020-03-05 21:12:27 -0500

    pushing add steps activity

--- app/src/main/java/org/byui/meg21allred/thymesavor/NewRecipeActivity.java
+++ app/src/main/java/org/byui/meg21allred/thymesavor/NewRecipeActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class NewRecipeActivity extends AppCompatActivity {
@@ -14,6 +15,8 @@ public class NewRecipeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_new_recipe);
 
         addStepsActitivyBtn = (Button) findViewById(R.id.addStepsActivityBtn);
